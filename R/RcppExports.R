1410e8d3b1dcbe0b5e9947a56f93f254e60dde04
Min Ni Hong <mhon0009@student.monash.edu>
2025-06-05 21:25:11 +1000

    Save local changes to Blackjack.so

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,6 +2,6 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 score_hand_cpp <- function(ranks) {
-    .Call('_Blackjack_score_hand_cpp', PACKAGE = 'Blackjack', ranks)
+    .Call(`_Blackjack_score_hand_cpp`, ranks)
 }
 
